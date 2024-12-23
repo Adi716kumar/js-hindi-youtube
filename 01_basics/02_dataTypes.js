83c3ccfe89350ffa6f4f9eb5496ff6aa97c2a715
ADITYA KUMAR <adityasrivastava716@gmail.com>
2024-12-23 13:36:04 +0000

    Data  types and conversion done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,5 +1,24 @@
 "use strict";// treat all JS code as newer version
 
-// alert("hello")
+// alert("hello")   alert will not give error if we run file in browser
 // alert(3+3)  we are using nodejs, not browser
-console.log(3+3); console.log("Aditya")
+console.log(3+3)
+console.log("Aditya")
+
+let name = "aditya"
+let age = 18
+let isLoggedIn = false
+
+// number --> 2 to 2^53
+//bigint for large numbers
+//string => "..."
+//boolean => true/false
+//null => standalone value -->used when we do not want to return any value
+//undefined => empty no value
+//symbol => unique
+
+//OBJECT
+
+console.log(typeof age); // typeof-->gives the inf abbout data type
+console.log(typeof null); // type of null is object.
+console.log(typeof undefined); // type of undefined is undefined.
---
+++ 01_basics/03_conversionoroperation.js
@@ -0,0 +1,18 @@
+let score = "3abc"
+console.log(typeof score) // string type
+
+// to Number()conversion.
+let valueinNumber = Number(score) //converted into number
+console.log(typeof valueinNumber)  // number type
+console.log(valueinNumber)  //gives NaN not a number because 3abc in NaN 
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+// null => 0
+//Note-> if we convert null type to number it will print 0.
+
+// to String conversion.
+let x = 56
+let strnumber = String(x)   // gives 56 not "56",
+console.log(strnumber)
+console.log(typeof strnumber)
