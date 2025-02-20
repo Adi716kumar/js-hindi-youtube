45f7dd69e7b8c5a776e98b44fffc700f4fe15660
ADITYA KUMAR <adityasrivastava716@gmail.com>
2025-02-20 05:19:54 +0000

    conversion finished

--- 01_basics/03_conversionoroperation.js
+++ 01_basics/03_conversionoroperation.js
@@ -22,3 +22,33 @@ let strnumber = String(x)   // gives 56 not "56",
 console.log(strnumber)
 console.log(typeof strnumber)
 
+
+
+// **********************operations ************************
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+let str1 = "aditya "
+let str2 = "kumar"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);  // 12
+console.log(1+"2");  // 12
+console.log("1"+2+2); // 122
+console.log(1+2+"2"); // 32
+
+console.log(true);  //true
+console.log(+true);   //1
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2;
+console.log(num1,num2,num3);   // 4 4 4
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter)
+
+// link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
