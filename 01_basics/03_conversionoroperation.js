55a9daad6475cfd25b5684b9dfb80a16d3a5337e
ADITYA KUMAR <adityasrivastava716@gmail.com>
2025-02-20 04:59:07 +0000

    conversion of datatypes

--- 01_basics/03_conversionoroperation.js
+++ 01_basics/03_conversionoroperation.js
@@ -6,13 +6,19 @@ let valueinNumber = Number(score) //converted into number
 console.log(typeof valueinNumber)  // number type
 console.log(valueinNumber)  //gives NaN not a number because 3abc in NaN 
 // "33" => 33
-// "33abc" => NaN
+// "33abc" => NaN   => but type is number                     
 // true => 1; false =>0
 // null => 0
 //Note-> if we convert null type to number it will print 0.
+
+//to boolean conversion
+// 1=> true, 0 = false
+//"" => false
+//"aditya" => true
 
 // to String conversion.
 let x = 56
 let strnumber = String(x)   // gives 56 not "56",
 console.log(strnumber)
 console.log(typeof strnumber)
+
