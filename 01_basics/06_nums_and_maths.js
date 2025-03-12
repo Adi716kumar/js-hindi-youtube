c33646d944766378f4ef6fe881b49da4d5ffd1d7
ADITYA KUMAR <adityasrivastava716@gmail.com>
2025-03-12 06:19:32 +0000

    light went off

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,25 @@
+const score = 400
+console.log(score);
+
+const balance  =  new Number(100)
+console.log(balance);
+
+const y = balance.toString()
+console.log(typeof y);
+console.log(y.length)
+
+console.log(balance.toFixed(2));
+
+const num = 1123.896
+console.log(num.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+//+++++++++++++++++++Math++++++++++++++++++++
+//built library in js
+console.log(Math);
+console.log(Math.abs(-98));
+console.log(Math.round(9.6))
+console.log(Math.ceil(8.4))
+console.log(Math.floor(8.4))
